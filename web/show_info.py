2688ec468f9aa1cc3be4ad94bc2261bd4b9f24c4
GulsahKose <gulsah.1004@gmail.com>
2014-07-08 15:07:03 +0300

    Changed show_info func because changed Proc_info class

--- web/show_info.py
+++ web/show_info.py
@@ -1,29 +1,33 @@
 import os
 import sys
-sys.path.append("../lib")
 from flask import Flask, render_template
 import getProcInfo 
+import ConfigParser
+import xmlrpclib
+
+CFILE = "/etc/supervisord-centralized.conf"
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+cfg = ConfigParser.ConfigParser()
+cfg.read(CFILE)
+user = cfg.get('DEFAULT', 'user')
+password = cfg.get('DEFAULT', 'password')
+host = cfg.get('DEFAULT', 'host')
+port = cfg.get('DEFAULT', 'port')
+address="http://%s:%s@%s:%s/RPC2" %(user, password, host, port)
+server = xmlrpclib.Server(address)
+lis = server.supervisor.getAllProcessInfo()
+info_list = []
+
 @app.route('/')
 def show_info():
-    obj = getProcInfo.Proc_info("long5_script")
-    name = obj.name
-    group = obj.group
-    start = obj.start
-    stop = obj.stop
-    now = obj.now
-    state = obj.state
-    statename = obj.statename
-    spawnerr = obj.spawnerr
-    exitstatus = obj.exitstatus
-    stdoutlog = obj.stdout_logfile
-    stderrlog = obj.stderr_logfile
-    pid = obj.pid
-    info_list = (name, group, start, stop, now, state, statename, spawnerr, exitstatus, stdoutlog, stderrlog, pid)
-    return render_template('show_info.html', info_list=info_list)
+    lis = server.supervisor.getAllProcessInfo()
+    for i in lis:
+        one = getProcInfo.Proc_info(i)
+        info_list.append(one)
+    return render_template('show_info.html', info_list = info_list)
             
            # name=name, group=group, start=start, stop=stop,\
            # now=now, state=state, statename=statename, spawnerr=spawnerr, exitstatus=exitstatus,\

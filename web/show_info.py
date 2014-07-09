76ffb5ca841c08236ea1c4eaf837c06bf873efd9
GulsahKose <gulsah.1004@gmail.com>
2014-07-09 14:03:55 +0300

    Added stop and restart

--- web/show_info.py
+++ web/show_info.py
@@ -18,5 +18,19 @@ def show_info():
         info_list.append(one)
     return render_template('show_info.html', info_list = info_list)
 
+@app.route('/process/<event>')
+def action(event):
+    if event == "restart":
+#        <?php $proc_name = _POST['restart'] ?>
+        server.supervisor.stopProcess(proc_name)
+        server.supervisor.startProcess(proc_name)
+        
+    if event == "stop":
+#        <?php $proc_name = _POST['stop'] ?>
+        server.supervisor.stopProcess(proc_name)
+
+
+
+
 if __name__ == '__main__':
     app.run()

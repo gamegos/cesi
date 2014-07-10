09973b4afb1ca47f0248bc2d882c06d93d9f8205
GulsahKose <gulsah.1004@gmail.com>
2014-07-10 11:00:50 +0300

    Fixed start stop and restart buttons for nodes

--- web/show_info.py
+++ web/show_info.py
@@ -20,23 +20,23 @@ def show_info():
 @app.route('/node/<node_name>')
 def nodelist(node_name):
     node = Node(node_name)
-    return render_template('show_info.html', process_list = node.process_list)
+    return render_template('show_info.html', process_list = node.process_list, node_name = node_name)
 
-@app.route('/process/stop/<process_name>')
-def stopProcess(process_name):
-    connection.supervisor.stopProcess(process_name)
-    return redirect(url_for('show_info')) 
+@app.route('/node/<node_name>/process/stop/<process_name>')
+def stopProcess(node_name, process_name):
+    Node(node_name).connection.supervisor.stopProcess(process_name)
+    return redirect(url_for('nodelist', node_name = node_name)) 
 
-@app.route('/process/start/<process_name>')
-def startProcess(process_name):
-    connection.supervisor.startProcess(process_name)
-    return redirect(url_for('show_info'))
+@app.route('/node/<node_name>/process/start/<process_name>')
+def startProcess(node_name, process_name):
+    Node(node_name).connection.supervisor.startProcess(process_name)
+    return redirect(url_for('nodelist', node_name = node_name)) 
 
-@app.route('/process/restart/<process_name>')
-def restartProcess(process_name):
-    connection.supervisor.stopProcess(process_name)
-    connection.supervisor.startProcess(process_name)
-    return redirect(url_for('show_info'))
+@app.route('/node/<node_name>/process/restart/<process_name>')
+def restartProcess(node_name, process_name):
+    Node(node_name).connection.supervisor.stopProcess(process_name)
+    Node(node_name).connection.supervisor.startProcess(process_name)
+    return redirect(url_for('nodelist', node_name = node_name)) 
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)

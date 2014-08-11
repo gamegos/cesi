9f6a297c039b4897f052f8646ce6fb5eec11e286
GulsahKose <gulsah.1004@gmail.com>
2014-08-11 16:15:40 +0300

    Provide get log information when button click

--- web/static/custom.js
+++ web/static/custom.js
@@ -280,24 +280,22 @@ var $select = function(){
                                    //Readlog
                                    $tr_p.append('<td><a class="btn btn-primary btn-block act" name="/node/'+$node_name+'/process/'+result['process_info'][$counter]['group']+':'+result['process_info'][$counter]['name']+'/readlog"> Readlog </a></td>');
                                    $readlog = $tr_p.children('td').last().children('a').first();
-                                   $logdiv.append('<div class="'+$node_name+'_'+result['process_info'][$counter]['group']+':'+result['process_info'][$counter]['name']+'">aaaaaaaaaaaaaaaaaaaa</div>');
-                                   $dia = $logdiv.children('div').last()
-                                   $dia.dialog({
-                                            dialogClass:'dialog_style1',
-                                            autoOpen: false,
-                                            show: {
-                                                effect: "blind",
-                                                duration: 1000
-                                            },
-                                            hide: {
-                                                effect: "explode",
-                                                duration: 1000
-                                        }
+                                   $dialogstr = '<div class="'+$node_name+'_'+result['process_info'][$counter]['group']+':'+result['process_info'][$counter]['name']+'">Yes I am a log </div>' 
+                                    $readlog.click(function(){
+                                        $logdiv.append($dialogstr);
+                                        $dia = $logdiv.children('div').last()
+                                        $dia.dialog({
+                                                dialogClass:'dialog_style1',
+                                                show: {
+                                                    effect: "blind",
+                                                    duration: 1000
+                                                },
+                                                hide: {
+                                                    effect: "explode",
+                                                    duration: 1000
+                                                }
+                                        });
                                     });
-                                   
-                                $readlog.click(function(){
-                                     $dia.dialog( "open" );
-                                });
                                     
                                 }
                 
@@ -388,24 +386,26 @@ var $showallprocess = function(){
                                    //Readlog
                                    $tr_p.append('<td><a class="btn btn-primary btn-block act" name="/node/'+nodename+'/process/'+result['process_info'][$counter]['group']+':'+result['process_info'][$counter]['name']+'/readlog"> Readlog </a></td>');
                                    $readlog = $tr_p.children('td').last().children('a').first();
-                                   $logdiv.append('<div class="'+nodename+'_'+result['process_info'][$counter]['group']+':'+result['process_info'][$counter]['name']+'">ssssssssssssssssssssssssssssssssssssssssssss</div>');
-                                   $dia = $logdiv.children('div').last()
-                                  
-                                   $dia.dialog({
-                                            dialogClass:'dialog_style1',
-                                            autoOpen: false,
-                                            show: {
-                                                effect: "blind",
-                                                duration: 1000
-                                            },
-                                            hide: {
-                                                effect: "explode",
-                                                duration: 1000
-                                        }
-                                    });
-                                
+                                   $dialogstr = '<div class="'+$node_name+'_'+result['process_info'][$counter]['group']+':'+result['process_info'][$counter]['name']+'">Yes I am a log aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>' 
                                     $readlog.click(function(){
-                                        $dia.dialog( "open" );
+                                        $logdiv.append($dialogstr);
+                                        $dia = $logdiv.children('div').last()
+                                        $dia.dialog({
+                                                width: 600,
+                                                heigth: 800,
+                                                resizable: true,
+                                                show: {
+                                                    effect: "blind",
+                                                    duration: 1000
+                                                },
+                                                hide: {
+                                                    effect: "explode",
+                                                    duration: 1000
+                                                }
+                                        });
+                                       $(function() {
+                                            $dia.draggable();
+                                       });
                                     });
                                 }
                         }

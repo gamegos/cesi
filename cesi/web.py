1d853e97a9e6dcd96e65cae93990009766a6608f
Gustavo Kawamoto <gkawamoto@valemobi.com.br>
2016-08-31 21:29:06 -0300

    Showing GitHub's logo when auth_mode is github

--- cesi/web.py
+++ cesi/web.py
@@ -97,7 +97,8 @@ def control():
 # Render login page
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    cesicfg = Config(CONFIG_FILE)
+    return render_template('login.html', auth_mode=cesicfg.getAuthMode())
 
 # Logout action
 @app.route('/logout', methods = ['GET', 'POST'])

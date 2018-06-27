c4205b5c8a212bbcc9fed9ea4d507cf654a94547
pleycpl <h4rvey@protonmail.com>
2018-06-27 10:29:20 +0300

    Add JsonValue class

--- cesi/util.py
+++ cesi/util.py
@@ -1,5 +1,7 @@
 import logging
 
+from flask import jsonify
+
 class ActivityLog:
     __instance = None
 
@@ -25,3 +27,32 @@ class ActivityLog:
         if ActivityLog.__instance == None:
             ActivityLog()
         return ActivityLog.__instance
+
+class JsonValue:
+
+    @staticmethod
+    def success(node=None,
+                process_name="",
+                event_name="",
+                status="Success",
+                code=80):
+
+        return jsonify(status=status,
+                       code=code,
+                       message = f"{node.name} {process_name} {event_name} event succesfully",
+                       nodename = node.name,
+                       data = node.get_process(process_name))
+
+    @staticmethod
+    def error(node=None,
+            process_name="",
+            event_name="",
+            status="Error",
+            code=0,
+            payload=""):
+
+        return jsonify(status=status,
+                       code=code,
+                       message = f"{node.name} {process_name} {event_name} event unsuccesfully",
+                       nodename = node.name,
+                       payload=payload)

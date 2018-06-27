683aca734717cf72f720638f32233951b98f9c6a
pleycpl <h4rvey@protonmail.com>
2018-06-27 12:02:17 +0300

    Create 'users' blueprint

---
+++ cesi/blueprints/users/routes.py
@@ -0,0 +1,115 @@
+from flask import (
+    Blueprint,
+    jsonify,
+    session,
+    request
+)
+
+from core import (
+    Cesi
+)
+from decorators import (
+    is_user_logged_in,
+    is_admin
+)
+from util import (
+    ActivityLog,
+    get_db
+)
+
+users = Blueprint('users', __name__)
+cesi = Cesi.getInstance()
+activity = ActivityLog.getInstance()
+
+@users.route('/', methods=['GET'])
+@is_user_logged_in()
+@is_admin("Unauthorized user request for delete user event. Delete user event fail.")
+def user_list():
+    cur = get_db().cursor()
+    cur.execute("select username, type from userinfo")
+    result = cur.fetchall()
+    users = [ {'name': str(element[0]), 'type': str(element[1])} for element in result]
+    return jsonify(status = 'success',
+                    users = users)
+
+@users.route('/<username>/delete', methods=["DELETE"])
+@is_user_logged_in("Illegal request for delete {username} user event.")
+@is_admin("{user} is unauthorized user for request to delete {username} user. Delete event fail.")
+def delete_user(username):
+    if username != "admin":
+        cur = get_db().cursor()
+        cur.execute("delete from userinfo where username=?",[username])
+        get_db().commit()
+        activity.logger.error("{} user deleted.".format(session['username']))
+        return jsonify(status = "success")
+    else:
+        activity.logger.error("{} user request for delete admin user. Delete admin user event fail.".format(session['username']))
+        return jsonify(status = "error",
+                        message= "Admin can't delete")
+
+# Writes new user information to database
+@users.route('/add', methods = ['POST'])
+@is_user_logged_in("Illegal request for add user event.")
+@is_admin("{user} is unauthorized user for request to add user event. Add user event fail.")
+def adduserhandler():
+    username = request.form['username']
+    password = request.form['password']
+    confirmpassword = request.form['confirmpassword']
+    try:
+        usertype = int(request.form['usertype'])
+    except ValueError as e:
+        return jsonify(status="warning",
+                        message=str(e))
+
+    if username == "" or password == "" or confirmpassword == "":
+        return jsonify( status = "null",
+                        message = "Please enter value")
+    else:
+        cur = get_db().cursor()
+        cur.execute("select * from userinfo where username=?",(username,))
+        if not cur.fetchall():
+            if password == confirmpassword:
+                cur.execute("insert into userinfo values(?, ?, ?)", (username, password, usertype,))
+                get_db().commit()
+                activity.logger.error("New user added({}).".format(session['username']))
+                return jsonify(status = "success",
+                                message ="User added")
+            else:
+                # Is it necessary?
+                activity.logger.error("Passwords didn't match at add user event.")
+                return jsonify(status = "warning",
+                                message ="Passwords didn't match")
+        else:
+            activity.logger.error("Username is not available. Please select different username")
+            return jsonify(status = "warning",
+                            message ="Username is not available. Please select different username")
+
+@users.route('/<username>/password', methods=['PUT'])
+@is_user_logged_in("Illegal request for change {username}'s password event.")
+def change_password(username):
+    if session['username'] == username:
+        old_password = request.form['old']
+        new_password = request.form['new']
+        confirm_password = request.form['confirm']
+        cur = get_db().cursor()
+        cur.execute("select password from userinfo where username=?",(username,))
+        ar=[str(r[0]) for r in cur.fetchall()]
+        if old_password == ar[0]:
+            if new_password == confirm_password:
+                if new_password != "":
+                    cur.execute("update userinfo set password=? where username=?",[new_password, username])
+                    get_db().commit()
+                    activity.logger.error("{} user change own password.".format(session['username']))
+                    return jsonify(status = "success")
+                else:
+                    return jsonify(status = "null",
+                                    message = "Please enter valid value")
+            else:
+                activity.logger.error("Passwords didn't match for {} 's change password event. Change password event fail.".format(session['username']))
+                return jsonify(status = "error", message = "Passwords didn't match")
+        else:
+            activity.logger.error("Old password is wrong for {} 's change password event. Change password event fail.".format(session['username']))
+            return jsonify(status = "error", message = "Old password is wrong")
+    else:
+        activity.logger.error("{} user request to change {} 's password. Change password event fail.".format(session['username'], username))
+        return jsonify(status = "error", message = "You can only change own password.")
--- cesi/web.py
+++ cesi/web.py
@@ -23,7 +23,8 @@ from decorators import (
 )
 from util import (
     ActivityLog,
-    JsonValue
+    JsonValue,
+    get_db
 )
 
 VERSION = "v2"
@@ -33,13 +34,6 @@ app.config.from_object(__name__)
 
 cesi = None
 activity = None
-
-# Database connection
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = cesi.get_db_connection()
-    return db
 
 # Close database connection
 @app.teardown_appcontext
@@ -51,7 +45,7 @@ def close_connection(_):
 @app.route(f'/{VERSION}/userinfo')
 @is_user_logged_in()
 def user_info():
-    return jsonify(username=session['username'], usertypecode=session['usertype'])
+    return jsonify(username=session['username'], usertypecode=session['usertypecode'])
 
 # Render login page or username, password control
 @app.route('/login', methods = ['GET', 'POST'])
@@ -73,7 +67,7 @@ def login():
                 session['username'] = username
                 session['logged_in'] = True
                 cur.execute("select * from userinfo where username=?",(username,))
-                session['usertype'] = cur.fetchall()[0][2]
+                session['usertypecode'] = cur.fetchall()[0][2]
                 activity.logger.info("{} logged in.".format(session['username']))
                 return redirect('/')
             else:
@@ -91,100 +85,6 @@ def logout():
     session.clear()
     return redirect('/login')
 
-@app.route(f'/{VERSION}/users')
-@is_user_logged_in()
-@is_admin("Unauthorized user request for delete user event. Delete user event fail.")
-def user_list():
-    cur = get_db().cursor()
-    cur.execute("select username, type from userinfo")
-    result = cur.fetchall()
-    users = [ {'name': str(element[0]), 'type': str(element[1])} for element in result]
-    return jsonify(status = 'success',
-                    users = users)
-
-@app.route(f'/{VERSION}/delete/user/<username>')
-@is_user_logged_in("Illegal request for delete {username} user event.")
-@is_admin("{user} is unauthorized user for request to delete {username} user. Delete event fail.")
-def del_user_handler(username):
-    if username != "admin":
-        cur = get_db().cursor()
-        cur.execute("delete from userinfo where username=?",[username])
-        get_db().commit()
-        activity.logger.error("{} user deleted.".format(session['username']))
-        return jsonify(status = "success")
-    else:
-        activity.logger.error("{} user request for delete admin user. Delete admin user event fail.".format(session['username']))
-        return jsonify(status = "error",
-                        message= "Admin can't delete")
-
-# Writes new user information to database
-@app.route(f'/{VERSION}/add/user/handler', methods = ['GET', 'POST'])
-@is_user_logged_in("Illegal request for add user event.")
-@is_admin("{user} is unauthorized user for request to add user event. Add user event fail.")
-def adduserhandler():
-    username = request.form['username']
-    password = request.form['password']
-    confirmpassword = request.form['confirmpassword']
-
-    if username == "" or password == "" or confirmpassword == "":
-        return jsonify( status = "null",
-                        message = "Please enter value")
-    else:
-        if request.form['usertype'] == "Admin":
-            usertype = 0
-        elif request.form['usertype'] == "Standart User":
-            usertype = 1
-        elif request.form['usertype'] == "Only Log":
-            usertype = 2
-        elif request.form['usertype'] == "Read Only":
-            usertype = 3
-
-        cur = get_db().cursor()
-        cur.execute("select * from userinfo where username=?",(username,))
-        if not cur.fetchall():
-            if password == confirmpassword:
-                cur.execute("insert into userinfo values(?, ?, ?)", (username, password, usertype,))
-                get_db().commit()
-                activity.logger.error("New user added({}).".format(session['username']))
-                return jsonify(status = "success",
-                                message ="User added")
-            else:
-                # Is it necessary?
-                activity.logger.error("Passwords didn't match at add user event.")
-                return jsonify(status = "warning",
-                                message ="Passwords didn't match")
-        else:
-            activity.logger.error("Username is not available. Please select different username")
-            return jsonify(status = "warning",
-                            message ="Username is not available. Please select different username")
-
-@app.route(f'/{VERSION}/change/password/<username>/handler', methods=['POST'])
-@is_user_logged_in("Illegal request for change {username}'s password event.")
-def changepasswordhandler(username):
-    if session['username'] == username:
-        cur = get_db().cursor()
-        cur.execute("select password from userinfo where username=?",(username,))
-        ar=[str(r[0]) for r in cur.fetchall()]
-        if request.form['old'] == ar[0]:
-            if request.form['new'] == request.form['confirm']:
-                if request.form['new'] != "":
-                    cur.execute("update userinfo set password=? where username=?",[request.form['new'], username])
-                    get_db().commit()
-                    activity.logger.error("{} user change own password.".format(session['username']))
-                    return jsonify(status = "success")
-                else:
-                    return jsonify(status = "null",
-                                    message = "Please enter valid value")
-            else:
-                activity.logger.error("Passwords didn't match for {} 's change password event. Change password event fail.".format(session['username']))
-                return jsonify(status = "error", message = "Passwords didn't match")
-        else:
-            activity.logger.error("Old password is wrong for {} 's change password event. Change password event fail.".format(session['username']))
-            return jsonify(status = "error", message = "Old password is wrong")
-    else:
-        activity.logger.error("{} user request to change {} 's password. Change password event fail.".format(session['username'], username))
-        return jsonify(status = "error", message = "You can only change own password.")
-
 @app.route(f'/{VERSION}/initdb')
 def initdb():
     cesi.drop_database()
@@ -195,21 +95,13 @@ def initdb():
 def showMain():
     # get user type
     if session.get('logged_in'):
-        if session['usertype']==0:
-            usertype = "Admin"
-        elif session['usertype']==1:
-            usertype = "Standart User"
-        elif session['usertype']==2:
-            usertype = "Only Log"
-        elif session['usertype']==3:
-            usertype = "Read Only"
-
+        username = session['username']
+        usertypecode = session['usertypecode']
         return render_template('index.html',
                                 name = cesi.name,
                                 theme = cesi.theme,
-                                username = session['username'],
-                                usertype = usertype,
-                                usertypecode = session['usertype'])
+                                username = username,
+                                usertypecode = usertypecode)
     else:
          return redirect('/login')
 
@@ -234,14 +126,17 @@ if __name__ == '__main__':
     activity = ActivityLog(log_path=cesi.activity_log)
     app.secret_key = cesi.secret_key
     
+    # or dynamic import
     from blueprints.nodes.routes import nodes
     from blueprints.activitylogs.routes import activitylogs
     from blueprints.environments.routes import environments
     from blueprints.groups.routes import groups
+    from blueprints.users.routes import users
     app.register_blueprint(nodes, url_prefix=f"/{VERSION}/nodes")
     app.register_blueprint(activitylogs, url_prefix=f"/{VERSION}/activitylogs")
     app.register_blueprint(environments, url_prefix=f"/{VERSION}/environments")
     app.register_blueprint(groups, url_prefix=f"/{VERSION}/groups")
+    app.register_blueprint(users, url_prefix=f"/{VERSION}/users")
     
     app.run(
         host=cesi.host,

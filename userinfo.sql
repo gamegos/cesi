5c57d81a25c14b852b5bd35f8148c88b90ea11de
pleycpl <h4rvey@protonmail.com>
2018-07-19 16:08:05 +0300

    Delete unnessary sql file

--- userinfo.sql
+++
@@ -1,3 +0,0 @@
-drop table if exists userinfo;
-create table userinfo(username varchar(30) PRIMARY KEY NOT NULL, password varchar(50) NOT NULL, type INT NOT NULL);
-insert into userinfo values('admin', 'admin', 0);

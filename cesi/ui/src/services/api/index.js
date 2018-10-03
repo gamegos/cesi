8a5d3b306a6799cc23ecebfe1304df575f091ab4
pleycpl <h4rvey@protonmail.com>
2018-10-03 18:00:08 +0300

    Get only 20 activity logs

--- cesi/ui/src/services/api/index.js
+++ cesi/ui/src/services/api/index.js
@@ -37,7 +37,7 @@ const deleteRequest = url => {
 };
 
 const activitylogs = {
-  get: () => getRequest("/v2/activitylogs/"),
+  get: () => getRequest("/v2/activitylogs/20/"),
   clear: () => deleteRequest("/v2/activitylogs/")
 };
 

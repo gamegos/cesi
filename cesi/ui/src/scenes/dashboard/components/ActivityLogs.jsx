d183bf11858e4d7a7644a3a1bb4f61a44529dbf9
pleycpl <h4rvey@protonmail.com>
2018-10-03 18:14:37 +0300

    Add reverse button for activity logs

--- cesi/ui/src/scenes/dashboard/components/ActivityLogs.jsx
+++ cesi/ui/src/scenes/dashboard/components/ActivityLogs.jsx
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import { Row, Col, Button } from "reactstrap";
 
 import { ActivityLogList } from "scenes/dashboard/components";
 
-const ActivityLogs = ({ logs, refreshLogs, clearLogs }) => {
-  return (
-    <React.Fragment>
-      <br />
-      <Row>
-        <Col sm="10">
-          <h2>Activity Logs</h2>
-        </Col>
-        <Col>
-          <Button outline color="info" onClick={refreshLogs}>
-            Refresh
-          </Button>
-          <Button outline color="danger" onClick={clearLogs}>
-            Clear
-          </Button>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <ActivityLogList logs={logs} />
-        </Col>
-      </Row>
-    </React.Fragment>
-  );
-};
+class ActivityLogs extends Component {
+  state = {
+    isReverse: false
+  };
+
+  reverse = () => {
+    this.setState((prevstate, _) => ({
+      isReverse: !prevstate.isReverse
+    }));
+  };
+  render() {
+    let logs = this.props.logs;
+    if (this.state.isReverse) {
+      logs = logs.reverse();
+    }
+
+    return (
+      <React.Fragment>
+        <br />
+        <Row>
+          <Col sm="9">
+            <h2>Activity Logs</h2>
+          </Col>
+          <Col>
+            <Button outline color="warning" onClick={this.reverse}>
+              Reverse
+            </Button>{" "}
+            <Button outline color="info" onClick={this.props.refreshLogs}>
+              Refresh
+            </Button>{" "}
+            <Button outline color="danger" onClick={this.props.clearLogs}>
+              Clear
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ActivityLogList logs={logs} />
+          </Col>
+        </Row>
+      </React.Fragment>
+    );
+  }
+}
 
 export default ActivityLogs;

7268f20f2b39be7d4cccaff34b2696b1ec504caf
Fatih Sarhan <f9n@protonmail.com>
2018-11-09 12:29:51 +0300

    Support postgresql database

--- cesi/models.py
+++ cesi/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16), index=True, unique=True)
-    password = db.Column(db.String(64))
+    password = db.Column(db.String(120))
     usertype = db.Column(db.Integer)
 
     def set_password(self, password):
--- cesi/utils.py
+++ cesi/utils.py
@@ -1,2 +1,2 @@
 def convert_user_object_to_dict(user):
-    return {"name": user.username, "type": str(user.usertype)}
+    return {"name": user.username, "type": user.usertype}

aa270bdbd3e80af536d88adbc7950e622a141008
f9n <f9n@protonmail.com>
2020-06-28 17:05:36 +0300

    Use one 'if' block for checking username and password emptiness

--- cesi/core/xmlrpc.py
+++ cesi/core/xmlrpc.py
@@ -1,19 +1,21 @@
 from urllib.parse import urlparse, urlunparse
 import xmlrpc.client
 
+
 class XmlRpc:
     @staticmethod
     def connection(host, port, username, password):
-        if not (host.startswith('http://') or host.startswith('https://')):
-            host = 'http://' + host
+        if not (host.startswith("http://") or host.startswith("https://")):
+            host = "http://" + host
         scheme, netloc, path, params, query, fragment = urlparse(host)
-        path = path.rstrip('/')
+        path = path.rstrip("/")
         server = "{}:{}".format(netloc, port)
-        credentials = "{}:{}".format(username, password)
-        if username == "" and password == "":
-            uri = urlunparse((scheme, server, path + '/RPC2', params, query, fragment))
-        else:
-            uri = urlunparse((scheme, '{credentials}@{server}'.format(credentials=credentials, server=server), path + '/RPC2', params, query, fragment))
+        if not (username == "" and password == ""):
+            server = "{username}:{password}@{server}".format(
+                username=username, password=password, server=server
+            )
+
+        uri = urlunparse((scheme, server, path + "/RPC2", params, query, fragment))
 
         print(uri)
         return xmlrpc.client.ServerProxy(uri)
